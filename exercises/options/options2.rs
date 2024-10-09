3599a547c32901760626edc94dd18963a7785187
dingyutong <ytding1999@163.com>
2024-10-09 17:07:10 +0800

    till options2

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -13,7 +13,10 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+        match optional_target {
+            opptional_targe::word =>
+        }
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
